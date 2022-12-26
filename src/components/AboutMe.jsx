991309b5807f9e806e3394aeef20da7432cbe088
Joana <joanamdsantos1@gmail.com>
2022-12-26 13:31:15 -0300

    feat: adicionando foto e texto no componente

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import './AboutMe.css'
+import './AboutMe.css';
+import joana from '../images/joana-photo.jpeg';
 
 function AboutMe() {
   return (
     <header className="header-container">
-      <h1>Joana Maria dos Santos</h1>
-      <p>woman_teacher arrow_right woman_technologist Atualmente estou em transição de carreira, saindo da área da educação para a área da tecnologia. Decidi me aventurar em uma nova área por desejar atuar em algo que me permita sair da comodidade e me desafiar cada dia mais e acredito que faço isso constantemente na área de desenvolvimento.
-
-book Iniciei meus estudos para me tornar Desenvolvedora Full Stack na Trybe em julho de 2022.
-
-musical_note Adoro música e estou constantemente escutando principalmente música pop
-
-mage_woman No meu tempo livre gosto muito de assistir séries, filmes e ler livros, principalmente de ficção científica, aventura e fantasia.</p>
+      <div className="info-aboutme">
+      <h1 className="title-name">Joana Maria dos Santos, 25 anos</h1>
+      <h3 className="subtitle-who">Quem é a Joana?</h3>
+      <p className="text-aboutme">Estou em transição de carreira, saindo da área da educação para a área da tecnologia. Decidi me aventurar em uma nova área por desejar atuar em algo que me permita sair da comodidade e me desafiar cada dia mais e acredito que faço isso constantemente na área de desenvolvimento. 
+      Comecei meus estudos para me tornar desenvolvedora full stack na Trybe em julho de 2022. Já terminei o módulo de fundamentos do desenvolvimento web, recentemente finalizei o módulo de front-end e no próximo ano iniciarei o módulo de back-end. 
+      Adoro música e estou constantemente escutando principalmente música pop.
+      No meu tempo livre gosto muito de assistir séries, filmes e ler livros, principalmente de ficção científica, aventura e fantasia.</p>
+      </div>
+      <div className="my-picture">
+        <img src={ joana } alt="imagem da Joana" className="joana-picture"/>
+      </div>
     </header>
   );
 }
