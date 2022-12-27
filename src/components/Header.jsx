8085b6357266e8a26570a966d0fe8bfa4c39888c
Joana <joanamdsantos1@gmail.com>
2022-12-27 16:08:19 -0300

    feat: header finalizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,30 @@
+import { ButtonGroup } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-import { FaHome } from 'react-icons/fa';
+import { FaArrowLeft, FaHome, FaPhone } from 'react-icons/fa';
+import { useHistory } from 'react-router-dom';
+import './Header.css'
 
 function Header() {
+  const history = useHistory();
+
   return (
-    <Navbar bg="light">
+      <Navbar bg="light" className="header-container">
       <Container>
-        <Navbar.Brand href="/">
+        <ButtonGroup onClick={ () => history.goBack() }>
+          <FaArrowLeft />
+        </ButtonGroup>
+        <ButtonGroup onClick={() => history.push('/')}>
           <FaHome />
+        </ButtonGroup>
+        <Navbar.Brand>
+          Portfólio de Joana Maria dos Santos
         </Navbar.Brand>
+        <ButtonGroup onClick={() => history.push('/contact')}>
+          <FaPhone />
+        </ButtonGroup>
       </Container>
-    </Navbar>
+      </Navbar>
   );
 }
 
