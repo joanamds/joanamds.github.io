d5149709d044e3c7d344d94dc740be0f75e65a79
Joana <joanamdsantos1@gmail.com>
2022-12-29 17:57:20 -0300

    começando a edição da página contato

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Header from '../components/Header';
+import '../styles/Contact.css';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { MdEmail } from "react-icons/md";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 function Contact() {
   return (
     <div className="contact-page">
       <Header />
-      <h1>Contact</h1>
+      <h1>Contato</h1>
+        <Card style={{ width: '30rem' }}>
+          <ListGroup variant="flush">
+          <ListGroup.Item><MdEmail /></ListGroup.Item>
+            <ListGroup.Item><FaLinkedin /></ListGroup.Item>
+            <ListGroup.Item><FaGithub /></ListGroup.Item>
+          </ListGroup>
+        </Card>
     </div>
   );
 }
