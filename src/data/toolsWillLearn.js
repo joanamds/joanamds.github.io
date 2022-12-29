e53d1fa52789857fe3d8f5ad8d1ed2bf7ddf901f
Joana <joanamdsantos1@gmail.com>
2022-12-29 16:06:16 -0300

    adicionando os dados do que aprendi e do que vou aprender

---
+++ src/data/toolsWillLearn.js
@@ -0,0 +1,35 @@
+import Python from '../icons/python.svg';
+import Node from '../icons/node.svg';
+import MongoDB from '../icons/mongoDB.svg';
+import MySQL from '../icons/mysql.svg';
+import TypeScript from '../icons/typescript.svg';
+import SQL from '../icons/sql.png';
+
+const toolsWillLearn = [
+  {
+    icon: SQL,
+    title: 'SQL',
+  },
+  {
+    icon: MySQL,
+    title: 'MySQL',
+  },
+  {
+    icon: TypeScript,
+    title: 'TypeScript',
+  },
+  {
+    icon: Node,
+    title: 'Node',
+  },
+  {
+    icon: MongoDB,
+    title: 'MongoDB',
+  },
+  {
+    icon: Python,
+    title: 'Python',
+  },
+];
+
+export default toolsWillLearn;
