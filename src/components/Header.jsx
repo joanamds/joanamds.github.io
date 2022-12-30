46d83cf01a010e8a7fb2d4776025d495683c37b0
Joana <joanamdsantos1@gmail.com>
2022-12-30 14:48:21 -0300

    feat: adicionando tooltips

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,24 +27,38 @@ function Header() {
   return (
       <Navbar bg={ isDarkTheme? 'dark' : 'light'} className="header-container">
       <Container>
-        <ButtonGroup onClick={ () => history.goBack() }>
-          <FaArrowLeft />
-        </ButtonGroup>
+          <ButtonGroup onClick={ () => history.goBack() }>
+            <FaArrowLeft />
+            <span className="tooltiptext">Voltar</span>
+          </ButtonGroup>
         <ButtonGroup onClick={() => history.push('/')}>
           <FaHome />
+          <span className="tooltiptext">Voltar ao início</span>
         </ButtonGroup>
         <Navbar.Brand>
           Portfólio
         </Navbar.Brand>
         <ButtonGroup onClick={() => history.push('/contact')}>
           <FaPhone />
+          <span className="tooltiptext">Contato</span>
         </ButtonGroup>
         <ButtonGroup
           className="theme"
           onClick={ toggleTheme }
         >
-          {isDarkTheme && <BsSunFill style={ { color: 'yellow' } } />}
-          {!isDarkTheme && <BsMoonFill style={ { color: 'blue'}} />}
+          {isDarkTheme && (
+            <>
+              <BsSunFill style={ { color: 'yellow' } } />
+              <span className="tooltiptext">Ativar o modo claro</span>
+            </>
+          )}
+          {!isDarkTheme
+            && (
+            <>
+              <BsMoonFill style={{ color: 'blue' }} />
+              <span className="tooltiptext">Ativar o modo escuro</span>
+            </>
+            )}
         </ButtonGroup>
       </Container>
       </Navbar>
