642c811e6a9cb7fe04fb245e2fb44812b5a44c7c
Joana <joanamdsantos1@gmail.com>
2022-12-30 12:41:23 -0300

    adicionando email no contato

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from '../components/Header';
 import '../styles/Contact.css';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import { MdEmail } from "react-icons/md";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
+import ThemeContext from '../context/ThemeContext';
 
 function Contact() {
+  const { isDarkTheme } = useContext(ThemeContext);
+
   return (
     <div className="contact-page">
       <Header />
       <h1>Contato</h1>
-        <Card style={{ width: '30rem' }}>
-          <ListGroup variant="flush">
-          <ListGroup.Item><MdEmail /></ListGroup.Item>
-            <ListGroup.Item><FaLinkedin /></ListGroup.Item>
-            <ListGroup.Item><FaGithub /></ListGroup.Item>
+      <Card style={{ width: '30rem' }} variant={ isDarkTheme ? 'dark' : 'light'}>
+        <ListGroup>
+          <ListGroup.Item variant={isDarkTheme ? 'dark' : 'light'}>
+            <div className="email-item">
+              <MdEmail style={{ color: '#F14133' }} />
+              <p className="email">joanamdsantos1@gmail.com</p>
+            </div>
+          </ListGroup.Item>
+          <ListGroup.Item variant={isDarkTheme ? 'dark' : 'light'}><FaLinkedin style={{ color: '#0078B8' }} /></ListGroup.Item>
+          <ListGroup.Item variant={isDarkTheme ? 'dark' : 'light'}><FaGithub style={{ color: 'black' }} /></ListGroup.Item>
           </ListGroup>
         </Card>
     </div>
